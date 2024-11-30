6ee33dca10547125aa2d55cead34abee71a61429
NexalyOrellana <70233776+NexalyOrellana@users.noreply.github.com>
2024-11-30 13:43:50 -0800

    Delete user button
    Added delete user functionality
    -button logic
    -query logic

--- src/UserAccount.js
+++ src/UserAccount.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import Navbar from './Navbar';
 import "./UserAccount.css";
+import { useNavigate } from 'react-router-dom';
 
 function UserAccount() {
     //Declare state variables
@@ -11,6 +12,7 @@ function UserAccount() {
     const [newPassword, setNewPassword] = useState("");
     const [confirmPassword, setConfirmPassword] = useState("");
     const [notificationEnabled, setNotificationEnabled] = useState(false);
+    const navigate = useNavigate();
 
     // Load user data from localStorage
     useEffect(() => {
@@ -57,98 +59,130 @@ function UserAccount() {
             alert('An error occurred while updating user information.');
         }
     };
+
+    const handleDeleteUser = async () => {
+        try{
+            
+            if(window.confirm("Press Ok if you are sure you want to delete your profile.\nThis action is permanent and cannot be reversed.")){
+                const response = await fetch('http://localhost:3001/delete-user',{
+                    method: 'DELETE',
+                    headers: {'Content-Type': 'application/json' },
+                    body: JSON.stringify({
+                        email
+                    })
+                });
+
+                const result = await response.json();
+                if(result.success){
+                    console.log("User: " + email + " was deleted successfully");
+                    alert(result.message);
+                    localStorage.removeItem('user');
+                    navigate('/');
+                }else{
+                    alert(result.message);
+                }
+            }
+        }catch (error){
+            console.error("Error deleting user:", error);
+            alert("An error occurred while attempting to delete your user profile, please try again later.");
+        }
+        
+    };
+
     
     
 
     return (
-        <div className='user-account-container'>
+        <>
             <Navbar isSignedIn={true} />
-            <main className='user-account-content'>
-            <h2 className= "section-title"> User Account</h2>
+            <div className='user-account-container'>
+                <main className='user-account-content'>
+                <h2 className= "section-title"> User Account</h2>
 
-            <div className='side-by-side-sections'>
-            <div className='user-account-section'>
-                <h3 className='section-title'>User Details</h3>
-                <div className='form-group'>
-                    <label>First Name: </label>
-                    <div className='editable-field'>
-                    <input
-                        type="text"
-                        value={firstName}
-                        onChange={(e) => setFirstName(e.target.value)} 
-                    />
-                    <span className='edit-icon'>&#9998;</span> {/*Edit icon*/}
+                <div className='side-by-side-sections'>
+                <div className='user-account-section'>
+                    <h3 className='section-title'>User Details</h3>
+                    <div className='form-group'>
+                        <label>First Name: </label>
+                        <div className='editable-field'>
+                        <input
+                            type="text"
+                            value={firstName}
+                            onChange={(e) => setFirstName(e.target.value)} 
+                        />
+                        <span className='edit-icon'>&#9998;</span> {/*Edit icon*/}
+                        </div>
+                    </div>
+                    <div className='form-group'>
+                        <label>Last Name: </label>
+                        <div className='editable-field'>
+                        <input
+                            type="text"
+                            value={lastName}
+                            onChange={(e) => setLastName(e.target.value)} 
+                        />
+                        <span className='edit-icon'>&#9998;</span> {/*Edit icon*/}
+
+                    </div>
+                    </div>
+                    <div>
+                        <label>Email: </label>
+                        <input
+                            type="email"
+                            value={email}
+                            readOnly
+                            className='readonly-input' 
+                        />
                     </div>
                 </div>
-                <div className='form-group'>
-                    <label>Last Name: </label>
-                    <div className='editable-field'>
-                    <input
-                        type="text"
-                        value={lastName}
-                        onChange={(e) => setLastName(e.target.value)} 
-                    />
-                    <span className='edit-icon'>&#9998;</span> {/*Edit icon*/}
 
-                </div>
-                </div>
-                <div>
-                    <label>Email: </label>
-                    <input
-                        type="email"
-                        value={email}
-                        readOnly
-                        className='readonly-input' 
-                    />
-                </div>
+                <div className='change-password-section'>
+                    <h3 className='section-title'>Change Password</h3>
+                        <div className='form-group'>
+                        <label>Current Password: </label>
+                        <input
+                            type="password"
+                            value={currentPassword}
+                            onChange={(e) => setCurrentPassword(e.target.value)} 
+                        />
+                        </div>
+                        <div className='form-group'>
+                        <label>New Password: </label>
+                        <input
+                            type="password"
+                            value={newPassword}
+                            onChange={(e) => setNewPassword(e.target.value)} 
+                        />
+                        </div>
+                        <div className='form-group'>
+                        <label>Confirm New Password: </label>
+                        <input
+                            type="password"
+                            value={confirmPassword}
+                            onChange={(e) => setConfirmPassword(e.target.value)} 
+                        />
+                        </div>
+                    </div>
+                    </div>
+
+                    <h2 className= 'section-title'> Notification Settings</h2>
+                        <div className='notification-section'>
+                        <label>ON/OFF:</label>
+                        <input
+                        type='checkbox'
+                        checked={notificationEnabled}
+                        onChange={() => setNotificationEnabled(!notificationEnabled)}
+                        />
+                        </div>
+
+                        {/*button style*/}
+                        <div className='buttons-container'>
+                            <button type='button' className='save-button' onClick={handleSaveChanges}>Save Changes</button>
+                            <button type='button' className='delete-button' onClick={handleDeleteUser}>Delete Profile</button>
+                        </div>
+                </main>
             </div>
-
-            <div className='change-password-section'>
-                <h3 className='section-title'>Change Password</h3>
-                    <div className='form-group'>
-                    <label>Current Password: </label>
-                    <input
-                        type="password"
-                        value={currentPassword}
-                        onChange={(e) => setCurrentPassword(e.target.value)} 
-                    />
-                    </div>
-                    <div className='form-group'>
-                    <label>New Password: </label>
-                    <input
-                        type="password"
-                        value={newPassword}
-                        onChange={(e) => setNewPassword(e.target.value)} 
-                    />
-                    </div>
-                    <div className='form-group'>
-                    <label>Confirm New Password: </label>
-                    <input
-                        type="password"
-                        value={confirmPassword}
-                        onChange={(e) => setConfirmPassword(e.target.value)} 
-                    />
-                    </div>
-                </div>
-                </div>
-
-                <h2 className= 'section-title'> Notification Settings</h2>
-                    <div className='notification-section'>
-                    <label>ON/OFF:</label>
-                    <input
-                    type='checkbox'
-                    checked={notificationEnabled}
-                    onChange={() => setNotificationEnabled(!notificationEnabled)}
-                    />
-                    </div>
-
-                    {/*button style*/}
-                    <div className='buttons-container'>
-                        <button type='button' className='save-button' onClick={handleSaveChanges}>Save Changes</button>
-                        <button type='button' className='delete-button'>Delete Profile</button>
-                    </div>
-            </main>
-        </div>
+        </>
     );
 }
 
--- src/server.js
+++ src/server.js
@@ -324,3 +324,22 @@ app.put('/update-user', (req, res) => {
   });
 });
 
+app.delete('/delete-user', (req, res) =>{
+  const {email} = req.body;
+  console.log('Email: ', email);
+  const query = 'DELETE FROM users WHERE email = ?';
+
+  db.query(query, [email], (err, result) =>{
+      if(err){
+        console.error('Error executing the delete query: ', err);
+        res.status(500).json({success: false, message: 'Failed to delete user profile, please try again later'});
+        return;
+      }
+      if(result.affectedRows === 0){
+        console.error('Delete cannot be performed: The user could not be found in the database')
+        res.status(404).json({success: false, message: 'The email could not be located in our database'});
+      }else{
+        res.status(200).json({success: true, message: 'The user profile associated with email: ' + email + ' has been deleted'});
+      }
+  });
+});
