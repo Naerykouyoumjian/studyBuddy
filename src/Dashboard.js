cfbc1ff432fa1eb0a961edec783478c5245c5d32
NexalyOrellana <70233776+NexalyOrellana@users.noreply.github.com>
2024-12-01 16:38:29 -0800

    Dashboard alerts and 404 page updates
    -Added more descriptive alerts to dashboard links, letting users know that certain features have not been implemented yet.
    
    -Centered 404 message on PageNotFound.js file

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -11,19 +11,19 @@ const Dashboard = () => {
       <div className="dashboard">
         <h1>User Dashboard</h1> 
         <div className="dashboard-content">
-          <div className="card" onClick={() => alert("Create Schedule")}>
+          <div className="card" onClick={() => alert("Create Schedule has been pressed.\nThis feature has not been implemented yet.\nConsider checking out our User Account page instead")}>
             <div className="icon">&#128197;</div>
             <div className="label">Create Schedule</div>
           </div>
-          <div className="card" onClick={() => alert("Create To-Do List")}>
+          <div className="card" onClick={() => alert("Create To-Do List has been pressed.\nThis feature has not been implemented yet.\nConsider checking out our User Account page instead")}>
             <div className="icon">&#128221;</div>
             <div className="label">Create To-Do List</div>
           </div>
-          <div className="card" onClick={() => alert("View Schedules")}>
+          <div className="card" onClick={() => alert("View Schedules has been pressed.\nThis feature has not been implemented yet.\nConsider checking out our User Account page instead")}>
             <div className="icon">&#128198;</div>
             <div className="label">View Schedules</div>
           </div>
-          <div className="card" onClick={() => alert("View To-Do Lists")}>
+          <div className="card" onClick={() => alert("View To-Do Lists has been pressed.\nThis feature has not been implemented yet.\nConsider checking out our User Account page instead")}>
             <div className="icon">&#128462;</div>
             <div className="label">View To-Do Lists</div>
           </div>
