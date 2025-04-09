d29fbe8ac4f5d18f343e0bb42df9af1ef8f3415c
Naery Kouyoumjian <naery.kouyoumjian.750@my.csun.edu>
2025-04-09 12:13:03 -0700

    Finalize the delete button functionality

--- src/ViewScheduleList.js
+++ src/ViewScheduleList.js
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import Navbar from "./Navbar";
 import "./ViewScheduleList.css";
-import SingleStudyPlan from './SingleStudyPlan';
 
 
 const ViewScheduleList = () => {
